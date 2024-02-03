d38f09efd7e4194d2d007ef2c5ecc97583ed1895
gokhansal <muhammed.gokhan55@gmail.com>
2024-02-04 00:31:40 +0300

    Edited Reset CSS and Variable CSS.

---
+++ instagram-clone/src/components/TextCmp.vue
@@ -0,0 +1,39 @@
+<template>
+    <component :is="tag" :class="size">
+        <slot></slot>
+    </component>
+</template>
+
+<script>
+export default {
+    props: {
+        tag: {
+            type: String,
+            default: 'span'
+        },
+        size: {
+            type: String,
+            default: 'normal', // normal:14, small:12, xsmall:11, xxsmall:10
+            validator(value) {
+                return ['normal', 'small', 'xsmall', 'xxsmall'].indexOf(value) != -1
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+.normal {}
+
+.small {
+    font-size: 12px;
+}
+
+.xsmall {
+    font-size: 11px;
+}
+
+.xxsmall {
+    font-size: 10px;
+}
+</style>
--- instagram-clone/src/views/HomeView.vue
+++ instagram-clone/src/views/HomeView.vue
@@ -1,9 +1,38 @@
-<script setup>
-
-</script>
-
 <template>
   <main>
     <h1>This is home page</h1>
+
+    <text-cmp>Hello World!</text-cmp>
+    <text-cmp class="a1" tag="p" size="small">Hello World!</text-cmp>
+    <text-cmp class="a2" tag="p" size="xsmall">Hello World!</text-cmp>
+    <text-cmp class="a3" tag="p" size="xxsmall">Hello World!</text-cmp>
   </main>
 </template>
+
+<script>
+import TextCmp from '@/components/TextCmp.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    // eslint-disable-next-line vue/no-unused-components
+    'text-cmp': TextCmp,
+  }
+}
+</script>
+
+<style scoped>
+.a1 {
+  color: rgba(var(--a72), 1);
+}
+
+.a2 {
+  color: rgba(var(--ba8), 1);
+  
+}
+
+.a3 {
+  color: rgba(var(--ba8), 1);
+  
+}
+</style>
