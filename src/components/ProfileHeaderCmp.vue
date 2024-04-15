a01868c86b2e6ac720012ae939cb30338e971b93
Gökhan SAL <95913491+ImagINN@users.noreply.github.com>
2024-04-15 23:43:04 +0300

    Update ProfileHeaderCmp.vue with new layout and styles

--- src/components/ProfileHeaderCmp.vue
+++ src/components/ProfileHeaderCmp.vue
@@ -53,33 +53,49 @@ export default {
 <template>
     <header class="z-90 fixed inset-x-0 top-0 bg-white">
         <main-container
-            class="header-inner h-44 flex items-center justify-center border-b border-solid border-slate-200 md:grid md:grid-cols-3 md:items-center my-4">
-            <div class="flex items-center justify-center mb-4">
-                <avatar class="items-center justify-center" :size="140" style="background: transparent;" />
+            class="header-inner h-44 flex items-center justify-center border-b border-solid border-slate-200 md:grid md:grid-cols-9 md:items-center my-4">
+            <div class="col-span-2 flex items-center justify-center mb-4">
+                <avatar class="flex items-center justify-center" :size="140" style="background: transparent;" />
             </div>
-            <div class="flex justify-start items-start">
-                <div class="grid grid-col-3 text-left items-start">
-                    <div class="flex justify-center items-center">
-                        <label class="font-semibold" for="">charlie_hunnam</label>
+            <div class="col-span-5">
+                <div class="grid grid-cols-4">
+                    <div class="col-span-2 mx-4">
+                        <div class="grid grid-rows-3">
+                            <label class="text-xl font-bold" for="">charlie_hunnam</label>
+                            <div class="grid grid-cols-3 mt-1">
+                                <label class="font-semibold" for="">12 gönderi</label>
+                                <label class="ml-1 font-semibold" for="">100 takipçi</label>
+                                <label class="ml-2 font-semibold" for="">123 takip</label>
+                            </div>
+                            <div class="grid grid-rows-2">
+                                <label for="">Charlie Hunnam</label>
+                                <label for="">Actor</label>
+                            </div>
+                        </div>
                     </div>
-                    <div class="">
-                        <button type="button">Profili Düzenle</button>
-                    </div>
-                    <div class="">
-                        <button>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
-                                <g fill="none" fill-rule="evenodd">
-                                    <path
-                                        d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
-                                    <path fill="currentColor"
-                                        d="M13.414 2.1a2 2 0 0 0-2.828 0L8.686 4H6a2 2 0 0 0-2 2v2.686l-1.9 1.9a2 2 0 0 0 0 2.828l1.9 1.9V18a2 2 0 0 0 2 2h2.686l1.9 1.9a2 2 0 0 0 2.828 0l1.9-1.9H18a2 2 0 0 0 2-2v-2.686l1.9-1.9a2 2 0 0 0 0-2.828l-1.9-1.9V6a2 2 0 0 0-2-2h-2.686zM12 3.516l1.9 1.9A2 2 0 0 0 15.313 6H18v2.686a2 2 0 0 0 .586 1.415l1.9 1.9l-1.9 1.899A2 2 0 0 0 18 15.314V18h-2.686a2 2 0 0 0-1.415.586l-1.9 1.9l-1.899-1.9A2 2 0 0 0 8.686 18H6v-2.686a2 2 0 0 0-.586-1.415L3.514 12l1.9-1.9A2 2 0 0 0 6 8.687V6h2.686a2 2 0 0 0 1.414-.586l1.9-1.9ZM10 12a2 2 0 1 1 4 0a2 2 0 0 1-4 0m2-4a4 4 0 1 0 0 8a4 4 0 0 0 0-8" />
-                                </g>
-                            </svg>
-                        </button>
+                    <div class="col-span-2 mx-4 text-white">
+                        <div class="flex justify-center items-center">
+                            <div class="grid grid-cols-3">
+                                <button class="w-32 h-8 col-span-2 rounded-lg bg-slate-500" type="button">
+                                    Profili Düzenle
+                                </button>
+                                <button
+                                    class="w-8 h-8 col-span-1 rounded-lg bg-slate-500 ml-2 flex justify-center items-center text-center">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24">
+                                        <g fill="none" fill-rule="evenodd">
+                                            <path
+                                                d="M24 0v24H0V0zM12.593 23.258l-.011.002l-.071.035l-.02.004l-.014-.004l-.071-.035c-.01-.004-.019-.001-.024.005l-.004.01l-.017.428l.005.02l.01.013l.104.074l.015.004l.012-.004l.104-.074l.012-.016l.004-.017l-.017-.427c-.002-.01-.009-.017-.017-.018m.265-.113l-.013.002l-.185.093l-.01.01l-.003.011l.018.43l.005.012l.008.007l.201.093c.012.004.023 0 .029-.008l.004-.014l-.034-.614c-.003-.012-.01-.02-.02-.022m-.715.002a.023.023 0 0 0-.027.006l-.006.014l-.034.614c0 .012.007.02.017.024l.015-.002l.201-.093l.01-.008l.004-.011l.017-.43l-.003-.012l-.01-.01z" />
+                                            <path fill="currentColor"
+                                                d="M13.414 2.1a2 2 0 0 0-2.828 0L8.686 4H6a2 2 0 0 0-2 2v2.686l-1.9 1.9a2 2 0 0 0 0 2.828l1.9 1.9V18a2 2 0 0 0 2 2h2.686l1.9 1.9a2 2 0 0 0 2.828 0l1.9-1.9H18a2 2 0 0 0 2-2v-2.686l1.9-1.9a2 2 0 0 0 0-2.828l-1.9-1.9V6a2 2 0 0 0-2-2h-2.686zM12 3.516l1.9 1.9A2 2 0 0 0 15.313 6H18v2.686a2 2 0 0 0 .586 1.415l1.9 1.9l-1.9 1.899A2 2 0 0 0 18 15.314V18h-2.686a2 2 0 0 0-1.415.586l-1.9 1.9l-1.899-1.9A2 2 0 0 0 8.686 18H6v-2.686a2 2 0 0 0-.586-1.415L3.514 12l1.9-1.9A2 2 0 0 0 6 8.687V6h2.686a2 2 0 0 0 1.414-.586l1.9-1.9ZM10 12a2 2 0 1 1 4 0a2 2 0 0 1-4 0m2-4a4 4 0 1 0 0 8a4 4 0 0 0 0-8" />
+                                        </g>
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <nav class="z-90 fixed left-0 right-0 bottom-0 bg-white border-t border-solid 
+            <nav class="col-span-2 z-90 fixed left-0 right-0 bottom-0 bg-white border-t border-solid 
       border-slate-100 h-48 flex items-center justify-around md:flex md:justify-end md:h-auto md:border-0 md:static">
                 <RouterLink to="/" class="mr-4 drop-shadow-md">
                     <icn-home-fill v-if="$route.name === 'Home'" />
